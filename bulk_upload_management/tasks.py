29ba3efdd3360a2ba9e3211e973a5b458cda8d12
sreerang k <sreerangk77@gmail.com>
2023-11-19 23:14:23 +0530

    final commit update requirements.txt

--- bulk_upload_management/tasks.py
+++ bulk_upload_management/tasks.py
@@ -6,6 +6,7 @@ from django.db import transaction
 
 
 
+
 @shared_task(bind=True)
 def test_func(self):
     for i in range(10):
@@ -18,26 +19,27 @@ def bulk_upload_products(self, data):
     processed_items = 0
 
     try:
-        for item in data:
-            try:
-                product = Product.objects.get(pk=item.get('id'))
-                # Update the  product
-                product.name = item.get('name')
-                product.description = item.get('description')
-                product.image = item.get('image')
-                product.price = item.get('price')
-                product.save()
-            except Product.DoesNotExist:
-                # Create a new product
-                Product.objects.create(
-                    name=item.get('name'),
-                    description=item.get('description'),
-                    image=item.get('image'),
-                    price=item.get('price')
-                )
-            # Update progress
-            processed_items += 1
-            self.update_state(state='PROGRESS')
+        with transaction.atomic():
+            for item in data:
+                try:
+                    product = Product.objects.get(pk=item.get('id'))
+                    # Update the existing product
+                    product.name = item.get('name')
+                    product.description = item.get('description')
+                    product.image = item.get('image')
+                    product.price = item.get('price')
+                    product.save()
+                except Product.DoesNotExist:
+                    # Create a new product
+                    Product.objects.create(
+                        name=item.get('name'),
+                        description=item.get('description'),
+                        image=item.get('image'),
+                        price=item.get('price')
+                    )
+                # Update progress
+                processed_items += 1
+                self.update_state(state='PROGRESS')
 
     except Exception as e:
         self.update_state(state='FAILURE', meta={'message': str(e)})
